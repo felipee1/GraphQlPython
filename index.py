18e9c64cc8b461354a2d976361aef1facf31c908
Felipe <felipe.stefani.correia@gmail.com>
2020-09-09 10:14:20 -0300

    Revert "first commit"
    This reverts commit cfc255c82ed6251bd822d5ea996757a478069e90.

--- index.py
+++
@@ -1,8 +0,0 @@
-import graphene
-from fastapi import FastAPI
-from starlette.graphql import GraphQLApp
-from schemas.schema import Query, Mutations
-from graphql.execution.executors.asyncio import AsyncioExecutor
-
-app = FastAPI()
-app.add_route("/", GraphQLApp(schema=graphene.Schema(query=Query, mutation=Mutations), executor_class=AsyncioExecutor))
